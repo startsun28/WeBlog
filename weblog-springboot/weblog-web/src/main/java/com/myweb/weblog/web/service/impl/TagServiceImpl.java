939e01f2612d69409b8b795812e8353ee58e1b07
liao <Liaown28@outlook.com>
2025-05-17 18:21:58 +0800

    Revert "获取标签文章页接口开发，标签文章页面开发，以及首页点击标签、文章实现跳转对应页面，标签、分类相关样式调整"
    This reverts commit fef858af950ba4666afa715d2fcab91c804ed233.

--- weblog-springboot/weblog-web/src/main/java/com/myweb/weblog/web/service/impl/TagServiceImpl.java
+++ weblog-springboot/weblog-web/src/main/java/com/myweb/weblog/web/service/impl/TagServiceImpl.java
@@ -2,20 +2,9 @@ package com.myweb.weblog.web.service.impl;
 
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.myweb.weblog.common.domain.dos.ArticleDO;
-import com.myweb.weblog.common.domain.dos.ArticleTagRelDO;
 import com.myweb.weblog.common.domain.dos.TagDO;
-import com.myweb.weblog.common.domain.mapper.ArticleMapper;
-import com.myweb.weblog.common.domain.mapper.ArticleTagRelMapper;
 import com.myweb.weblog.common.domain.mapper.TagMapper;
-import com.myweb.weblog.common.enums.ResponseCodeEnum;
-import com.myweb.weblog.common.exception.BizException;
-import com.myweb.weblog.common.utils.PageResponse;
 import com.myweb.weblog.common.utils.Response;
-import com.myweb.weblog.web.convert.ArticleConvert;
-import com.myweb.weblog.web.model.vo.tag.FindTagArticlePageListReqVO;
-import com.myweb.weblog.web.model.vo.tag.FindTagArticlePageListRspVO;
 import com.myweb.weblog.web.model.vo.tag.FindTagListRspVO;
 import com.myweb.weblog.web.service.TagService;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,10 +20,7 @@ public class TagServiceImpl implements TagService {
 
     @Autowired
     private TagMapper tagMapper;
-    @Autowired
-    private ArticleTagRelMapper articleTagRelMapper;
-    @Autowired
-    private ArticleMapper articleMapper;
+
     /**
      * 获取标签列表
      *
@@ -58,53 +43,6 @@ public class TagServiceImpl implements TagService {
         }
 
         return Response.success(vos);
-    }
-
-    /**
-     * 获取标签下文章分页列表
-     *
-     * @param findTagArticlePageListReqVO
-     * @return
-     */
-    @Override
-    public Response findTagPageList(FindTagArticlePageListReqVO findTagArticlePageListReqVO) {
-        Long current = findTagArticlePageListReqVO.getCurrent();
-        Long size = findTagArticlePageListReqVO.getSize();
-        // 标签 ID
-        Long tagId = findTagArticlePageListReqVO.getId();
-
-        // 判断该标签是否存在
-        TagDO tagDO = tagMapper.selectById(tagId);
-        if (Objects.isNull(tagDO)) {
-            log.warn("==> 该标签不存在, tagId: {}", tagId);
-            throw new BizException(ResponseCodeEnum.TAG_NOT_EXISTED);
-        }
-
-        // 先查询该标签下所有关联的文章 ID
-        List<ArticleTagRelDO> articleTagRelDOS = articleTagRelMapper.selectByTagId(tagId);
-
-        // 若该标签下未发布任何文章
-        if (CollectionUtils.isEmpty(articleTagRelDOS)) {
-            log.info("==> 该标签下还未发布任何文章, tagId: {}", tagId);
-            return PageResponse.success(null, null);
-        }
-
-        // 提取所有文章 ID
-        List<Long> articleIds = articleTagRelDOS.stream().map(ArticleTagRelDO::getArticleId).collect(Collectors.toList());
-
-        // 根据文章 ID 集合查询文章分页数据
-        Page<ArticleDO> page = articleMapper.selectPageListByArticleIds(current, size, articleIds);
-        List<ArticleDO> articleDOS = page.getRecords();
-
-        // DO 转 VO
-        List<FindTagArticlePageListRspVO> vos = null;
-        if (!CollectionUtils.isEmpty(articleDOS)) {
-            vos = articleDOS.stream()
-                    .map(articleDO -> ArticleConvert.INSTANCE.convertDO2TagArticleVO(articleDO))
-                    .collect(Collectors.toList());
-        }
-
-        return PageResponse.success(page, vos);
     }
 }
 
--- weblog-vue3/src/api/frontend/tag.js
+++ weblog-vue3/src/api/frontend/tag.js
@@ -4,8 +4,3 @@ import axios from "@/axios";
 export function getTagList(data) {
     return axios.post("/tag/list", data)
 }
-
-// 获取标签下文章列表
-export function getTagArticlePageList(data) {
-    return axios.post("/tag/article/list", data)
-}
