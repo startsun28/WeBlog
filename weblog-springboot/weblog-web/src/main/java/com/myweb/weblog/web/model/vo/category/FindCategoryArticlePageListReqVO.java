baf7e611d015a8f42d7378999d45a7a11cac30cb
liao <Liaown28@outlook.com>
2025-05-17 16:04:33 +0800

    获取某分类下的文章列表接口开发

---
+++ weblog-springboot/weblog-web/src/main/java/com/myweb/weblog/web/model/vo/category/FindCategoryArticlePageListReqVO.java
@@ -0,0 +1,21 @@
+package com.myweb.weblog.web.model.vo.category;
+
+import com.myweb.weblog.common.model.BasePageQuery;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindCategoryArticlePageListReqVO extends BasePageQuery {
+    /**
+     * 分类 ID
+     */
+    @NotNull(message = "分类 ID 不能为空")
+    private Long id;
+
+}
